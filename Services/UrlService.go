1e15ebbb4af5323425ac94d3622e4a1ebe316c57
sagar <sagar.dash290@gmail.com>
2021-10-02 01:06:21 +0600

    Update UrlService.go

--- Services/UrlService.go
+++ Services/UrlService.go
@@ -41,3 +41,54 @@ func GetLinks() []models.Link {
 	return links
 
 }
+
+func GetLink(link_id int) models.Link {
+	var link models.Link
+
+	result := models.Db.Where(&models.Link{Id: link_id}).First(&link)
+
+	if result.Error != nil {
+		return link
+	}
+
+	return link
+
+}
+
+func UpdateLink(url_id int, body structs.LinkBody) (bool, string) {
+	// var url models.Url
+	var message string
+
+	result := models.Db.Where(&models.Link{Id: url_id}).Updates(models.Link{
+		Title:               body.Title,
+		Url:                 body.Url,
+		UrlSelector:         body.UrlSelector,
+		DescriptionSelector: body.DescriptionSelector,
+	})
+
+	if result.Error != nil {
+		message = "cant update"
+		return false, message
+	}
+
+	message = "updated"
+
+	return true, message
+
+}
+
+func DeleteLink(id int) (bool, string) {
+	var link models.Link
+	var message string
+
+	result := models.Db.Where(&models.Link{Id: id}).Delete(&link)
+	if result.Error != nil {
+		message = "cant delete"
+		return false, message
+	}
+
+	message = "deleted"
+
+	return true, message
+
+}
