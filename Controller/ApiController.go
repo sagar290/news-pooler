46bc8658a3bd23e5b332b6090122e927b0ac5808
sagar <sagar.dash290@gmail.com>
2021-10-02 01:06:16 +0600

    Update ApiController.go

--- Controller/ApiController.go
+++ Controller/ApiController.go
@@ -4,6 +4,7 @@ import (
 	models "news-poolerr/Model"
 	services "news-poolerr/Services"
 	structs "news-poolerr/Structs"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -41,5 +42,58 @@ func GetLinks(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "",
 		"data":    links,
+	})
+}
+
+func GetLink(c *gin.Context) {
+	link_id := c.Param("feed_id")
+	var url models.Link
+
+	// convert link_id to int
+	id, err := strconv.Atoi(link_id)
+	if err != nil {
+
+		c.JSON(400, gin.H{
+			"message": "url_id type is not int",
+			"data":    nil,
+		})
+
+		return
+	}
+
+	url = services.GetLink(id)
+
+	c.JSON(200, gin.H{
+		"message": "",
+		"data":    url,
+	})
+}
+
+func DeleteLink(c *gin.Context) {
+
+	feed_id := c.Param("feed_id")
+
+	// convert feed_id to int
+	id, err := strconv.Atoi(feed_id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": "feed_id type is not int",
+			"data":    nil,
+		})
+		return
+	}
+
+	url, message := services.DeleteLink(id)
+
+	if !url {
+		c.JSON(400, gin.H{
+			"message": message,
+			"data":    nil,
+		})
+	}
+
+	c.JSON(200, gin.H{
+		"message": message,
+		"data":    nil,
 	})
 }
