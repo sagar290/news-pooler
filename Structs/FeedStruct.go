94b7f0dd1e539d5d383f145d8c321d5106859335
sagar <sagar.dash290@gmail.com>
2021-10-02 23:41:16 +0600

    Create FeedStruct.go

---
+++ Structs/FeedStruct.go
@@ -0,0 +1,9 @@
+package Structs
+
+type Feed struct {
+	Date     string `json:"date"`
+	Link_id  int    `json:"link_id"`
+	Url      string `json:"url"`
+	Title    string `json:"title"`
+	UniqueId string `json:"unique_id"`
+}
