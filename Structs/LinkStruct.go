6c60305528e22db849b751af6137d63b2bc96ced
sagar <sagar.dash290@gmail.com>
2021-10-02 23:41:18 +0600

    Update LinkStruct.go

--- Structs/LinkStruct.go
+++ Structs/LinkStruct.go
@@ -3,6 +3,7 @@ package Structs
 type LinkBody struct {
 	Title               string `json:"title" binding:"required"`
 	Url                 string `json:"url" binding:"required"`
+	Section             string `json:"section" binding:"required"`
 	UrlSelector         string `json:"url_selector" binding:"required"`
 	DescriptionSelector string `json:"description_selector" binding:"required"`
 	UniqueId            string `json:"unique_id"`
