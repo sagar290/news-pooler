52e2ffbfaf2a6fc91f33a3b5229b6a0f65115a8d
sagar <sagar.dash290@gmail.com>
2021-10-02 23:41:03 +0600

    Update Link.go

--- Model/Link.go
+++ Model/Link.go
@@ -5,13 +5,15 @@ import (
 )
 
 type Link struct {
-	Id                  int       `gorm:"primaryKey" json:"feed_id"`
+	Id                  int       `gorm:"primaryKey" json:"link_id"`
 	Title               string    `gorm:"size:255" json:"title"`
 	Thumbnail           string    `json:"thumbnail"`
 	Url                 string    `json:"url"`
+	Section             string    `gorm:"size:255" json:"section"`
 	UrlSelector         string    `gorm:"size:255" json:"url_selector"`
 	DescriptionSelector string    `gorm:"size:255" json:"description_selector"`
 	UniqueId            string    `gorm:"size:255" json:"unique_id"`
+	Feeds               []Feed    `gorm:"foreignKey:Link_id;references:Id; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	CreatedAt           time.Time `json:"created_at"`
 	UpdatedAt           time.Time `json:"updated_at"`
 }
