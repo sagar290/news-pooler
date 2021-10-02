9e5ae80ee7e69b7aacbc1a535c1e0ac12e171268
sagar <sagar.dash290@gmail.com>
2021-10-02 23:41:01 +0600

    Update Feed.go

--- Model/Feed.go
+++ Model/Feed.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Feed struct {
-	Id        int       `gorm:"primaryKey" json:"feed_id"`
-	Date      time.Time `json:"date"`
-	Url       string    `json:"url"`
+	Id        int    `gorm:"primaryKey" json:"feed_id"`
+	Date      string `gorm:"size:255" json:"date"`
+	Url       string `json:"url"`
+	Link_id   int
 	Title     string    `gorm:"size:255" json:"title"`
 	UniqueId  string    `gorm:"size:255" json:"unique_id"`
 	CreatedAt time.Time `json:"created_at"`
